9829464ce6c39430cd06a41f0e075349a12efbbb
bavar92 <vasilij6090@gmail.com>
2019-12-27 15:36:41 +0200

    2 commit

--- footer.php
+++ footer.php
@@ -7,3 +7,4 @@
 <?php wp_footer(); ?>
 </body>
 </html>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur ducimus expedita maxime recusandae tempora tenetur totam voluptates. Animi assumenda blanditiis, consectetur cupiditate dolore eveniet ex, explicabo fugiat iusto rerum soluta?
